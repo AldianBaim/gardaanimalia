f244976ab0cb81386a1ae7efbf8ac26f55fe0597
AldianBaim <aldiansyahibrahim5@gmail.com>
2024-12-21 18:46:44 +0700

    fix: move component navbar and footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
+// _app.js
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import NextNProgress from 'nextjs-progressbar';
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
@@ -7,5 +11,34 @@ export default function App({ Component, pageProps }) {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress color="#FF869A" height={3} />
+      <Navbar />
+      <main className="mx-4" style={{ paddingTop: "100px" }}>
+        <div className="row">
+          <div className="col-lg-2 position-relative">
+            <img
+              src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/tesso%20nillo.jpeg"
+              className="w-100 position-relative"
+              alt=""
+            />
+          </div>
+
+          <div className="col-lg-8">
+            <Component {...pageProps} />
+            <Footer />
+          </div>
+
+          <div className="col-lg-2">
+            <img
+              src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/tesso%20nillo.jpeg"
+              className="w-100"
+              alt=""
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,34 +1,12 @@
+// _document.js
 import { Html, Head, Main, NextScript } from "next/document";
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/Footer";
-import { useState } from "react";
 
 export default function Document() {
-
-  const [banner1, setBanner1] = useState(true)
-  const [banner2, setBanner2] = useState(true)
-
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Navbar/>
-        <main className="mx-4" style={{paddingTop: "100px"}}>
-          <div className="row">
-            <div className="col-lg-2 position-relative">
-              <img src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/tesso%20nillo.jpeg" className="w-100 position-relative" alt="" />
-            </div>
-
-            <div className="col-lg-8">
-              <Main />
-              <Footer/>
-            </div>
-
-            <div className="col-lg-2">
-              <img src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/tesso%20nillo.jpeg" className="w-100" alt="" />
-            </div>
-          </div>
-        </main>
+        <Main />
         <NextScript />
       </body>
     </Html>
