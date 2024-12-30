82ea21bd09c93834ebf666a0c06bc6aa9ec1869b
AldianBaim <aldiansyahibrahim5@gmail.com>
2024-12-30 14:41:11 +0700

    fix: convert html on page

--- src/pages/page/[page].js
+++ src/pages/page/[page].js
@@ -1,25 +1,30 @@
 
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 export async function getServerSideProps(context) {
-  const slug = context.params.page;
+  const slug = context.params.page
+  try {
+    // Fetch data from external API
+    const [postsRes, popularPostRes, pageRes] = await Promise.all([
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`),
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts/popular`),
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/pages/detail/${slug}`),
+    ]);
 
-  // Fetch data from external API
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/pages/detail/${slug}`)
-  const response = await res.json()
-  const data = response.data
+    const { data: posts } = await postsRes.json();
+    const { data: popularPosts } = await popularPostRes.json();
+    const { data: page } = await pageRes.json();
 
-  // Pass data to the page via props
-  return { props: { data } }
+    // Pass data to the page via props
+    return { props: { posts, popularPosts, page } };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { notFound: true };
+  }
 }
 
-export default function Page({data}) {
-  console.log(data)
-  
-
+export default function Page({posts, popularPosts, page}) {
 	return (
     <>
       <Head>
@@ -47,14 +52,12 @@ export default function Page({data}) {
         <meta name="twitter:data2" content="10 minutes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        
-      >
+      <div>
         <main >
           <img src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/Screenshot%202024-12-20%20at%2019.56.16.png" className="w-100 object-fit-cover mb-4" height={"300px"} alt="" />
           <div className="row">
             <div className="col-lg-8">
-              {data?.content}
+              <p className="lead" dangerouslySetInnerHTML={{ __html: page?.content }}></p>
             </div>
             <div className="col-lg-4">
               <div className="d-flex align-items-center mb-1">
@@ -62,55 +65,64 @@ export default function Page({data}) {
                 <div className="h6 m-0">Pos Terbaru</div>
               </div>
               <div className="mb-3 small text-muted">Baca berita terbaru seputar satwa liar di sini</div>
-              <div className="card position-relative mb-3">
-                <img src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/Screenshot%202024-12-20%20at%2020.03.11.png" className="w-100" style={{filter: "brightness(70%)"}} alt="" />
-                <div className="d-flex p-3 gap-2 position-absolute bottom-0">
-                  <div className="text-white">
-                    <div className="text-xs">Air dan Api Diserahkan ke BKSD</div>
-                    <div className="text-xs">11/11/2024</div>
+              <Link href={`/${posts[0]?.slug}`} className="text-decoration-none text-dark">
+                <div className="card position-relative mb-3 hover">
+                  <img src={posts[0]?.picture} className="w-100" style={{filter: "brightness(70%)"}} alt="" />
+                  <div className="d-flex p-3 gap-2 position-absolute bottom-0">
+                    <div className="text-white">
+                      <div className="text-xs">{posts[0]?.title}</div>
+                      <div className="text-xs">{posts[0]?.created_at}</div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              {[...Array(5)].map((item, index) => (
-                  <div key={index} className="row mb-3">
+              </Link>
+              {posts.slice(1).map((post, index) => (
+                <Link href={`/${post.slug}`} key={index} className="text-decoration-none text-dark">
+                  <div className="row mb-3 hover">
                     <div className="col-lg-3 pe-0">
-                      <img src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/Screenshot%202024-12-20%20at%2020.00.07.png" className="w-100" alt="" />
+                      <img src={post?.picture} className="w-100" alt="" />
                     </div>
                     <div className="col-lg-9">
-                      <div className="text-xs">Penyu Hijau Mati Terdampar di Pantai Legian, Diduga Dehidrasi</div>
-                      <div className="text-xs text-muted">11/11/2024</div>
+                      <div className="text-xs">{post.title}</div>
+                      <div className="text-xs text-muted">{post.created_at}</div>
                     </div>
                   </div>
-                ))
-              }
+                </Link>
+              ))}
+              {posts.length === 0 && <div className="text-center text-muted">Belum ada pos terbaru</div>}
               <div className="d-flex align-items-center mb-1 mt-5">
                 <div style={{width: "10px", height: "17px", backgroundColor: "#DB9723", borderRadius: "3px"}} className="me-2"></div>
                 <div className="h5 m-0">Bacaan Populer</div>
               </div>
               <div className="text-xs text-muted mb-3">Baca berita terbaru seputar satwa liar di sini</div>
-              <div className="card position-relative mb-3">
-                <img src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/Screenshot%202024-12-20%20at%2020.03.11.png" className="w-100" alt="" />
-                <div className="d-flex p-3 gap-2 position-absolute bottom-0" style={{background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5))'}}>
-                  <div className="d-flex align-items-center justify-content-center bg-orange text-white" style={{height: '80px', width: '140px'}}>1</div>
-                  <div className="text-white">
-                    <div className="text-xs">Jual konten penyiksaan monyet, pria asal Singkawan</div>
-                    <div className="text-xs">11/11/2024</div>
+              <Link href={`/${popularPosts[0]?.slug}`} className="text-decoration-none text-dark">
+                <div className="card position-relative mb-3 hover">
+                  <img src="https://ik.imagekit.io/8jggdaymrs/gardaanimalia/Screenshot%202024-12-20%20at%2020.03.11.png" className="w-100" alt="" />
+                  <div className="d-flex p-3 gap-2 position-absolute bottom-0" style={{background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5))'}}>
+                    <div className="d-flex align-items-center justify-content-center bg-orange text-white" style={{width: '140px'}}>1</div>
+                    <div className="text-white">
+                      <div className="text-xs text-ellipsis-2">{popularPosts[0]?.title}</div>
+                      <div className="text-xs">{popularPosts[0]?.created_at}</div>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
               {
-                [...Array(5)].map((item, index) => (
-                  <Link href="/satwa-liar" key={index} className="row mb-3 text-decoration-none text-dark hover">
-                    <div className="col-lg-3 pe-0">
-                      <div className="d-flex align-items-center justify-content-center text-white h-100" style={{background: '#D3442C'}}>1</div>
-                    </div>
-                    <div className="col-lg-9">
-                      <div className="text-xs">Penyu Hijau Mati Terdampar di Pantai Legian, Diduga Dehidrasi</div>
-                      <div className="text-xs text-muted">11/11/2024</div>
+                popularPosts.slice(1).map((post, index) => (
+                  <Link href={`/${post.slug}`} key={index} className="text-decoration-none text-dark">
+                    <div className="row mb-3 hover">
+                      <div className="col-lg-3 pe-0">
+                        <div className="d-flex align-items-center justify-content-center text-white h-100" style={{background: '#D3442C'}}>{(index + 1) + 1}</div>
+                      </div>
+                      <div className="col-lg-9">
+                        <div className="text-xs text-ellipsis-2">{post.title}</div>
+                        <div className="text-xs text-muted">{post.created_at}</div>
+                      </div>
                     </div>
                   </Link>
                 ))
               }
+              {popularPosts.length === 0 && <div className="text-center text-muted">Belum ada pos populer</div>}
             </div>
           </div>
         </main>
