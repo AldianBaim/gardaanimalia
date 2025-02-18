3be2f7fb29be9edecf287f07441f7849af6b6980
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-02-18 13:28:35 +0700

    feat: add meta google

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="aoQuV6CqqIzpG6H0BMsKYpl4Jk_bVUymCWJvgR52lGE" />
+      </Head>
       <body>
         <Main />
         <NextScript />
